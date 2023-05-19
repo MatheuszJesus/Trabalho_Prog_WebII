6b181121e4b61d36df8a6e3a4a9f0b7493acf79b
Matheus <130152350+MatheuszJesus@users.noreply.github.com>
2023-05-18 23:09:03 -0300

    Add files via upload

---
+++ paginaprincipal.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Secular+One&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Oswald&display=swap" rel="stylesheet">
+    <title>toystore</title>
+</head>
+<body>
+
+
+
+    <div id="header">
+            <img src="imgs/logo.png" alt="Logo do Site ToyStore Brinquedos" id="logo">
+
+            <div id="barra">
+                <a class="amarelo" id="empresa">Empresa</a>
+                <a class="amarelo" id="cupons">Cupons</a>
+                <a href="" class="amarelo" id="contato">Contato</a>
+                <a class="amarelo" id="carrinho">Carrinho</a>
+            </div>
+    </div>
+
+    <div id="content">
+        <div id="conteudopt1">
+            <h1 class="cortexto2" id="texto1">BRINQUEDOS INFANTIS</h1>
+            <h3 class="cortexto2" id="texto2">O melhor para sua criança, com praticidade, <br> segurança, higiene e preços<br> acessíveis.</h3>
+        </div>
+        <div id="conteudopt2">
+             <img src="imgs/margin_10px_-removebg-preview.png" alt="Imagem de um bebê com brinquedo" id="imgbebe">
+        </div>
+    </div>
+    <div id="footer">
+        <div id="textodorodape">
+            <h3 class="cortexto2" id="texto3">
+            Navegue pela nossa seleção com centenas<br> de brinquedos diferentes e escolha os preferidos.<br> Finalize o pedido e aproveite.
+            </h3>
+        </div>
+        <div id="botaoproduto">
+            <div id="quadrado">
+                <a href="" class="amarelo" id="produtos"> PRODUTOS</a>
+            </div>
+        </div>
+    </div>
+
+</body>
+</html>
+
