c0cc521dff8d44f49e02e99806047a3a29300666
Maria Eduarda <mariaeduardalimatias@gmail.com>
2023-05-20 21:25:56 -0300

    atualizado

--- paginaprodutos.php
+++ paginaprodutos.php
@@ -30,7 +30,7 @@
 
             <div id="barra">
                 <a href="paginaprincipal.php" class="amarelo" id="empresa">Empresa</a>
-                <a class="amarelo" id="cupons">Cupons</a>
+                <a href="" class="amarelo" id="produtos">Produtos</a>
                 <a href="" class="amarelo" id="contato">Contato</a>
                 <a class="amarelo" id="carrinho">Carrinho</a>
             </div>
