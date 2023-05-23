b29ef2658e7bbd00e1b53233ac0d597a3bcf6fb8
Jonathas de Almeida Lopes <132296304+Jon12345676@users.noreply.github.com>
2023-05-23 18:13:47 -0300

    Adicionando parte do cadastro

---
+++ cadastro.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Secular+One&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Oswald&display=swap" rel="stylesheet">
+    <title>toystore</title>
+</head>
+<body>
+
+
+
+    <div id="header">
+            <img src="img/logo.png" alt="Logo do Site ToyStore Brinquedos" id="logo">
+
+            <div id="barra">
+                <a class="amarelo" id="empresa">Empresa</a>
+                <a class="amarelo" id="cupons">Cupons</a>
+                <a href="" class="amarelo" id="contato">Contato</a>
+                <a class="amarelo" id="carrinho">Carrinho</a>
+            </div>
+    </div>
+
+    <div id="content">
+        <div id="conteudopt1">
+            <img id ="img-cad"src="img/bebeprinc.png">
+        </div>
+        <div id="conteudopt2">
+            <form action="" method="get">
+                    <label id="labels">Nome Completo:<br>
+                            <input type="text" id="nome" name="nome" required>
+                    </label>
+                    <br>
+                    <label id="labels">E-mail:<br>
+                            <input type="text" id="email" name="email" required>
+                    </label>
+                    <br>
+                    <label id="labels">Senha:<br>
+                            <input type="password" id="senha" name="senha" required>
+                    </label>
+                    <br>
+                    <label id="labels">Confirme a senha:<br>
+                            <input type="password" id="senha" name="senha" required>
+                    </label>
+                    <br>
+                    <button>REGISTRAR</button>
+                </form>
+        </div>
+    </div>
+    <!-- <div id="footer">
+		<h4>©2023 TOY STORE - Todos os direitos reservados.</h4>
+    </div> -->
+
+</body>
+</html>
